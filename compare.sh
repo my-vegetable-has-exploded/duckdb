0d3a08d4c9e5e34ea3ab1ae0615731e51532274b
my-vegetable-has-exploded <wy1109468038@gmail.com>
2024-10-23 20:02:30 +0800

    fix shell.

--- compare.sh
+++ compare.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # if this command fails, download the Transaktioner 2016 file manually at https://www.opendata.dk/city-of-aarhus/transaktionsdata-fra-aarhus-kommunes-biblioteker, and store it as books.csv
-# wget https://admin.opendata.dk/datastore/dump/787d0f74-42f9-407e-b52f-7d55e31ee762 -O books.csv
+wget https://admin.opendata.dk/datastore/dump/787d0f74-42f9-407e-b52f-7d55e31ee762 -O books.csv
 
 # transform the csv file to parquet format and single borrow record
 ./duckdb <books2016.sql
